dc330578d16c1d9a4348106f986a47f408c54784
Mandeep Singh <mandeep.singh@nagarro.com>
2018-05-15 14:58:44 +0530

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { StudentsComponent } from './students/students.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+import { TeachersComponent } from './teachers/teachers.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    StudentsComponent,
+    SubjectsComponent,
+    TeachersComponent
   ],
   imports: [
     BrowserModule
