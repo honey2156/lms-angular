4ceb84a0c27c0160e396d93f60e2b7da1b51fd92
Mandeep Singh <mandeep.singh@nagarro.com>
2018-05-15 23:43:38 +0530

    Till Add new batch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './/app-routing.module';
+import { BatchService } from './Service/batch.service';
+import { CourseService } from './Service/course.service';
+import { StudentService } from './Service/student.service';
+import { SubjectService } from './Service/subject.service';
+import { TeacherService } from './Service/teacher.service';
 import { AppComponent } from './app.component';
 import { BatchesComponent } from './batches/batches.component';
 import { CoursesComponent } from './courses/courses.component';
 import { StudentsComponent } from './students/students.component';
 import { SubjectsComponent } from './subjects/subjects.component';
 import { TeachersComponent } from './teachers/teachers.component';
-import { TeacherService } from './Service/teacher.service';
-import { StudentService } from './Service/student.service';
-import { CourseService } from './Service/course.service';
-import { SubjectService } from './Service/subject.service';
-import { BatchService } from './Service/batch.service';
-
 
 
 
@@ -29,7 +29,8 @@ import { BatchService } from './Service/batch.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [TeacherService, StudentService, CourseService, SubjectService, BatchService],
   bootstrap: [AppComponent]
